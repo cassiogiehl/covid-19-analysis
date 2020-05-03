6de36d10322a8c7d29ab4b4c0d52c1d323f0231a
Cassio Giehl <cassio.giehl@gmail.com>
2020-05-03 13:35:33 -0300

    insert state analysis

--- app.py
+++ app.py
@@ -5,36 +5,26 @@ import json
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-st.title('Coronavírus no RS')
+st.title('Coronavírus no Brasil')
 
 DATA_URL = 'https://brasil.io/dataset/covid19/caso?format=csv'
 NULL = '---'
 
 
-def load_data():
-    data = pd.read_csv(DATA_URL)
-    data = data.loc[data.place_type == 'city']
-    return data
-
-# page = st.sidebar.selectbox("Escolha uma opção:", ['Cidades', 'Opção 2', 'Opção 3'])
-
-# Cria um elemento na tela
-data_load_state = st.text('Carregando dados...')
-
-# Carrega 10k linhas no df
-data = load_data()
-
-# Muda o texto criado anteriormente
-data_load_state.text('Carregando dados... Pronto!')
+data = pd.read_csv(DATA_URL)
+data = data.loc[data.place_type == 'city']
 
 
 st.subheader('Dados Utilizados na análise')
 st.write(data)
 
+def mostra_grafico(df, x_label, y_label, titulo):
+	fig = px.line(df, x=x_label, y=y_label, title=titulo)
+	fig
 
-# Select de ESTADOS
 
-de_para_silga_estado = {
+# de para sigla -> estado
+de_para_sigla_estado = {
 	'AC': 'Acre',
 	'AL': 'Alagoas',
 	'AP': 'Amapá',
@@ -64,8 +54,8 @@ de_para_silga_estado = {
 	'TO': 'Tocantins'
 	}
 
-
-de_para_estado_silga = {
+# de para estado sigla
+de_para_estado_sigla = {
 	'Acre': 'AC',
 	'Alagoas': 'AL',
 	'Amapá': 'AP',
@@ -95,25 +85,46 @@ de_para_estado_silga = {
 	'Tocantins': 'TO'
 	}
 
+# buscando e convertendo sigla para estado
 estados = []
 estados.append(NULL)
 for estado in data['state'].sort_values().unique():
-	estados.append(de_para_silga_estado[estado])
+	estados.append(de_para_sigla_estado[estado])
 
+# select de estado
 estado_selecionado = st.selectbox(
     'Escolha o estado',
 	estados
 )
 
-'Estado selecionado: ', estado_selecionado
-
-
-
-
-
-# Select de CIDADES do ESTADO selecionado
+# mostra o estado selecionado
 if estado_selecionado is not NULL:
-	cidades_estado_selecionado = data['city'].loc[data.state == de_para_estado_silga[estado_selecionado]].sort_values().unique()
+	'Estado selecionado: ', estado_selecionado
+
+	# utilizando somente dados do estado selecionado
+	data = data.loc[data.state == de_para_estado_sigla[estado_selecionado]]
+
+
+	st.subheader('Análise do estado selecionado')
+
+    # dataframe de estado agregados pela data
+	df_estado = data.sort_values(by='date', ascending=True)
+
+    # dataframe de casos confirmados no estado selecionado
+	df_estado_confirmados = df_estado.groupby('date')[['confirmed']].sum().reset_index()
+	df_estado_confirmados.columns = ['data', f'qtd casos confirmados {estado_selecionado}']
+
+    # dataframe de casos de óbitos na cidade filtrada
+	df_estado_obitos = df_estado.groupby('date')['deaths'].sum().reset_index()
+	df_estado_obitos.columns = ['data', f'qtd de óbitos {estado_selecionado}']
+
+	mostra_grafico(df_estado_confirmados, 'data', f'qtd casos confirmados {estado_selecionado}', f'Casos confirmados em {estado_selecionado}')
+	
+	mostra_grafico(df_estado_obitos, 'data', f'qtd de óbitos {estado_selecionado}', f'Casos de óbitos em {estado_selecionado}')
+
+   
+    # Select de CIDADES do ESTADO selecionado
+	cidades_estado_selecionado = data['city'].sort_values().unique()
 	cidades = []
 
 	cidades.append(NULL)
@@ -125,28 +136,29 @@ if estado_selecionado is not NULL:
 		cidades
 	)
 
-	'Cidade selecionada: ', cidade_selecionada
-
-
-	# Filtrando a cidade selecionada na base de dados
-	df_cidade = data.loc[data['city'] == cidade_selecionada]
-	df_cidade.sort_values(by='date', ascending=True)
-
-	# dataframe de casos confirmados na cidade filtrada
-	df_cidade_confirmados = df_cidade[['date', 'confirmed']]
-	df_cidade_confirmados.columns = ['data', f'qtd casos confirmados {cidade_selecionada}']
-
-	# dataframe de casos de óbitos na cidade filtrada
-	df_cidade_obitos = df_cidade[['date', 'deaths']]
-	df_cidade_obitos.columns = ['data', f'qtd de óbitos {cidade_selecionada}']
-
-
-	# plot dos gráficos de quantidade de confirmados e óbitos por dia 
-	# na cidade filtrada
 	if cidade_selecionada is not NULL:
+		'Cidade selecionada: ', cidade_selecionada
 
-		fig = px.line(df_cidade_confirmados, x='data', y=f'qtd casos confirmados {cidade_selecionada}', title=f'Casos confirmados em {cidade_selecionada}')
-		fig
+		st.subheader('Análise da cidade selecionada')
+	
+		# Filtrando a cidade selecionada no dataframe do estado
+		df_cidade = data.loc[data['city'] == cidade_selecionada]
+		df_cidade.sort_values(by='date', ascending=True)
 
-		fig = px.line(df_cidade_obitos, x='data', y=f'qtd de óbitos {cidade_selecionada}', title=f'Casos de óbitos em {cidade_selecionada}')
-		fig
+		# dataframe de casos confirmados na cidade filtrada
+		df_cidade_confirmados = df_cidade.groupby('date')['confirmed'].sum().reset_index()
+		df_cidade_confirmados.columns = ['data', f'qtd casos confirmados {cidade_selecionada}']
+
+		# dataframe de casos de óbitos na cidade filtrada
+		df_cidade_obitos = df_cidade.groupby('date')['deaths'].sum().reset_index()
+		df_cidade_obitos.columns = ['data', f'qtd de óbitos {cidade_selecionada}']
+
+
+		# plot dos gráficos de quantidade de confirmados e óbitos por dia 
+		# na cidade filtrada
+		if cidade_selecionada is not NULL:
+			
+			mostra_grafico(df_cidade_confirmados, 'data', f'qtd casos confirmados {cidade_selecionada}', f'Casos confirmados em {cidade_selecionada}')
+
+			mostra_grafico(df_cidade_obitos, 'data', f'qtd de óbitos {cidade_selecionada}', f'Casos de óbitos em {cidade_selecionada}')
+			
